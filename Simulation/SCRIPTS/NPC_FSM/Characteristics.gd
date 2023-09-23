c2796e30c272d5aec776a4fac1f96095047dfb73
JimBimBomBom <95131004+JimBimBomBom@users.noreply.github.com>
2023-09-23 18:28:33 +0200

    Movement refactor - WORK IN PROGRESS

--- Simulation/SCRIPTS/NPC_FSM/Characteristics.gd
+++ Simulation/SCRIPTS/NPC_FSM/Characteristics.gd
@@ -10,14 +10,20 @@ var can_have_sex : bool
 
 #Locomotion
 var max_velocity : float
-var desired_velocity : Vector2
 var max_steering_force : float
-#Locomotion addons
-var time_step : float
-var time_value : float
-const MAX_ANGLE_CHANGE : float = 0.1
-var wander_noise = FastNoiseLite.new()
+var desired_velocity : Vector2 = (0, 0)
+#Locomotion - Wander variables
+var wander_jitter : float = 1.0
+var wander_radius : float = 20.0
+var wander_distance : float = 10.0
+var wander_target : Vector2 # needs to be initialized
 
+var threat_range : float
+
+var flock_behaviour_radius : float
+var separation_weight : float
+var cohesion_weight : float
+var alignment_weight : float
 
 #Base stats
 var mass : float
@@ -36,9 +42,6 @@ var hydration : float
 var hydration_norm : float
 var seek_hydration_norm : float = 0.3
 
-var separation_radius : float
-var cohesion_radius : float
-var alignment_radius : float
 
 func set_characteristics(genes : Animal_Genes):
 	age = World.Age_Group.JUVENILE # TODO option -> have age influence a variety of characteristics ... right now ignored
@@ -47,9 +50,10 @@ func set_characteristics(genes : Animal_Genes):
 
 	#Locomotion
 	max_velocity = (genes.musculature + (genes.metabolic_rate/2)) / (genes.size + World.velocity_start_point) # hate it
-	velocity = Vector2(0, 0)
-	acceleration = Vector2(0, 0)
 	max_steering_force = genes.musculature / genes.size
+
+	threat_range = genes.sense_range # TODO
+	flock_behaviour_radius = genes.sense_range/3 # TODO
 
 	#Base stats
 	mass = genes.size * 100
@@ -68,44 +72,108 @@ func set_characteristics(genes : Animal_Genes):
 	alignment_radius = 100
 
 func _ready():
-	# wander_noise.seed = randi()
-	wander_noise.seed = 1581123
+	wander_target = Vector2(wander_radius, 0).rotated(randf() * TAU)
 
 func get_tile_on_curr_pos() -> Vector2:
 	var result : Vector2i = position/World.tile_size
 	return Vector2(result.x, result.y)
 
-
 func do_move(delta : float) -> void:
-	var steering_force = desired_velocity - velocity 
+	var steering_force = (desired_velocity*max_velocity) - velocity # desired_velocity is always normalized -> *max_vel
 	steering_force.limit_length(max_steering_force)
 	velocity += steering_force
 	velocity.limit_length(max_velocity)
 	rotation = velocity.angle() + PI/2
 	position += velocity * delta #* World.animal_velocity_mult
 
-func wander(delta: float) -> Vector2:
-	# Increment wander_angle using noise
-	wander_angle += (noise.get_noise_2d(wander_angle, 0) - 0.5) * angle_change
-
-	var circle_x = position.x + wander_radius * cos(wander_angle)
-	var circle_y = position.y + wander_radius * sin(wander_angle)
-
-	# Get the desired direction towards the point on the circle
-	return Vector2(circle_x - position.x, circle_y - position.y).normalized()
-
-
-
-
-
-
-
-func move_calc(force : Vector2) -> void:
-	acceleration += force
-
 func seek(target : Vector2) -> Vector2:
-	var force = position.direction_to(target) * max_velocity
-	return (force - velocity).normalized()
+	var desired_velocity = position.direction_to(target) * max_velocity
+	return (desired_velocity - velocity).normalized()
+
+func flee(target : Vector2) -> Vector2:
+	var desired_velocity = target.direction_to(position) * max_velocity
+	return (desired_velocity - velocity).normalized()
+
+func wander() -> Vector2:
+	wander_target += Vector2(rand_range(-wander_jitter, wander_jitter), rand_range(-wander_jitter, wander_jitter))
+	wander_target = wander_target.normalized() * wander_radius
+	var target_world_position = position + (velocity.normalized() * wander_distance) + wander_target
+	return seek(target_world_position)
+
+func pursue(target: CharacterBody2D) -> Vector2:
+    var to_target = target.position - position
+    var relative_heading = velocity.normalized().dot(target.velocity.normalized())
+    
+    # Check if the target is ahead and facing the character with an angle less than 18 degrees
+    if to_target.dot(velocity.normalized()) > 0 and relative_heading < -0.95:  # cos(18°) is approximately 0.95
+        return seek(target.position)
+    
+    # Predict future position
+    var lookahead_time = to_target.length() / (max_velocity + target.velocity.length())
+    var predicted_target = target.position + target.velocity * lookahead_time
+    
+    return seek(predicted_target)
+
+func evade(pursuer: CharacterBody2D) -> Vector2:
+    var distance = pursuer.position - position
+    if distance.length2() > threat_range * threat_range:
+        return Vector2()  # No need to evade if pursuer is far away
+    
+    var estimated_time = distance.length() / max_velocity
+    var predicted_position = pursuer.position + pursuer.velocity * estimated_time
+    
+    return flee(predicted_position)
+
+func get_flee_dir(animals : Array[Animal]) -> Vector2:
+	var force : Vector2 = Vector2(0, 0)
+	for animal in animals:
+		var dist = abs(position.distance_to(animal.position))
+		var temp_force = evade(animal)
+		force += temp_force/dist
+	return force.normalized()
+
+func flock() -> Vector2:
+	var separation_force = separation(animals_of_same_type)
+	var alignment_force = alignment(animals_of_same_type)
+	var cohesion_force = cohesion(animals_of_same_type)
+	
+	var flocking_force = separation_force * separation_weight + alignment_force * alignment_weight + cohesion_force * cohesion_weight
+	return flocking_force
+
+func separation(animals) -> Vector2:
+	var force = Vector2()
+	for animal in animals:
+			var to_animal = position - animal.position
+			if to_animal.length() < flock_behaviour_radius:
+				force += to_animal.normalized() / to_animal.length()
+	return force
+
+func alignment(animals) -> Vector2:
+	var avg_velocity = Vector2()
+	var count = 0
+	for animal in animals:
+			var to_animal = position - animal.position
+			if to_animal.length() < flock_behaviour_radius:
+				avg_velocity += animal.velocity
+				count += 1
+	if count > 0:
+		avg_velocity /= count
+		avg_velocity = avg_velocity.normalized() * max_velocity
+		return avg_velocity - velocity
+	else:
+		return Vector2()
+
+func cohesion(animals) -> Vector2:
+	var center_mass = Vector2()
+	var count = 0
+	for animal in animals:
+			var to_animal = position - animal.position
+			if to_animal.length() < flock_behaviour_radius:
+				center_mass += animal.position
+				count += 1
+	if count > 0:
+		center_mass /= count
+	return seek(center_mass)
 
 func smooth_seek(target : Vector2) -> Vector2:
 	var force = seek(target)
@@ -114,8 +182,6 @@ func smooth_seek(target : Vector2) -> Vector2:
 		force *= dist/100
 	return force
 
-func flee(target : Vector2) -> Vector2:
-	return seek(target) * -1
 
 func pursue(target : Animal) -> Vector2:
 	var look_ahead_magnitude = position.distance_to(target.position) / 20
@@ -125,13 +191,6 @@ func pursue(target : Animal) -> Vector2:
 func evade(target : Animal) -> Vector2:
 	return pursue(target) * -1
 
-func get_flee_dir(animals : Array[Animal]) -> Vector2:
-	var force : Vector2 = Vector2(0, 0)
-	for animal in animals:
-		var dist = abs(position.distance_to(animal.position))
-		var temp_force = evade(animal)
-		force += temp_force/dist
-	return force.normalized()
 
 func get_separation_force(target: Animal):
 	var dist = abs(position.distance_to(target.position))
